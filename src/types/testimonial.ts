6cb75243f023f69076e1035ce86f146e59f9c160
elreco <elreco@users.noreply.github.com>
2025-06-15 17:27:41 +0200

    Update src/types/testimonial.ts - Version 0

---
+++ src/types/testimonial.ts
@@ -0,0 +1,11 @@
+export interface Testimonial {
+  id: string;
+  name: string;
+  role: string;
+  company: string;
+  companyLogo?: string;
+  profileImage: string;
+  rating: number;
+  feedback: string;
+  date: string;
+}
